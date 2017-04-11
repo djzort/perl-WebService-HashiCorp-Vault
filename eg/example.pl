944a5c1c45ca095442f630995f44a06a7d2cc88c
Dean Hamstead <dean@fragfest.com.au>
2017-04-11 21:57:24 +1000

    Add example file

---
+++ eg/example.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+$Data::Dumper::Sortkeys = 1;
+
+use lib 'lib';
+
+use WebService::HashiCorp::Vault;
+
+my $vault = WebService::HashiCorp::Vault->new( token => '94257c2a-a475-a301-cd33-2ead770de31b' );
+
+my $sys = $vault->sys;
+
+print Dumper $sys->health();
+print Dumper $sys->init();
+print Dumper $sys->leader();
+print Dumper $sys->mounts();
+print Dumper $sys->policy();
+print Dumper $sys->seal_status();
