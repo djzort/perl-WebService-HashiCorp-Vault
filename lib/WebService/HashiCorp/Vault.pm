dcdc2e7f73d83cad21e6fa66c43962686c97074a
Dean Hamstead <dean@fragfest.com.au>
2017-04-17 22:06:13 +1000

    Stray whitespace gone

--- lib/WebService/HashiCorp/Vault.pm
+++ lib/WebService/HashiCorp/Vault.pm
@@ -48,7 +48,7 @@ sub BUILD {
 
 A perl API for convenience in using HashiCorp's Vault server software.
 
-Vault secures, stores, and tightly controls access to tokens, passwords, certificates, API keys, and other secrets in modern computing. Vault handles leasing, key revocation, key rolling, and auditing. Through a unified API, users can access an encrypted Key/Value store and network encryption-as-a-service, or generate AWS IAM/STS credentials, SQL/NoSQL databases, X.509 certificates, SSH credentials, and more. 
+Vault secures, stores, and tightly controls access to tokens, passwords, certificates, API keys, and other secrets in modern computing. Vault handles leasing, key revocation, key rolling, and auditing. Through a unified API, users can access an encrypted Key/Value store and network encryption-as-a-service, or generate AWS IAM/STS credentials, SQL/NoSQL databases, X.509 certificates, SSH credentials, and more.
 
 =head1 ALPHA STATUS WARNING
 
